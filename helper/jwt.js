ee6c6bce09088f9550fd7722afa55f9ff2c8b221
Parul-0520 <100shparul@gmail.com>
2026-04-04 16:32:16 +0530

    Fix: Updated SMTP port and added TLS for Render deployment

--- helper/jwt.js
+++ helper/jwt.js
@@ -8,23 +8,24 @@ function authJwt() {
     }).unless({
         path: [
             // Public Routes
-            { url: /\/uploads(.*)/, methods: ['GET', 'OPTIONS'] }, // Images dikhane ke liye
+            { url: /\/uploads(.*)/, methods: ['GET', 'OPTIONS'] }, 
             { url: /\/api\/products(.*)/, methods: ['GET', 'OPTIONS'] },
             { url: /\/api\/category(.*)/, methods: ['GET', 'OPTIONS'] },
-            { url: /\/api\/cart(.*)/, methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'] }, // Cart operations allow kiye
+            { url: /\/api\/cart(.*)/, methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'] }, 
             
             // Auth Routes
             { url: /\/api\/user\/signin(.*)/, methods: ['POST', 'OPTIONS'] },
             { url: /\/api\/user\/signup(.*)/, methods: ['POST', 'OPTIONS'] },
-      
-{ url: /\/api\/productReviews(.*)/, methods: ['GET', 'POST', 'OPTIONS'] },
-{ url: /\/api\/my-list(.*)/, methods: ['GET', 'POST', 'DELETE', 'OPTIONS'] },
-{ url: /\/api\/orders(.*)/, methods: ['GET', 'POST', 'OPTIONS'] },
-{ url: /\/api\/search(.*)/, methods: ['GET', 'OPTIONS'] },
-{ url: /\/api\/user\/google-login(.*)/, methods: ['POST', 'OPTIONS'] },
-{ url: /\/api\/user\/verifyemail(.*)/, methods: ['POST', 'OPTIONS'] },
-{ url: /\/api\/user\/forgotPassword/, methods: ['POST'] },
-{ url: /\/api\/user\/resetPassword/, methods: ['POST'] },
+            { url: /\/api\/productReviews(.*)/, methods: ['GET', 'POST', 'OPTIONS'] },
+            { url: /\/api\/my-list(.*)/, methods: ['GET', 'POST', 'DELETE', 'OPTIONS'] },
+            { url: /\/api\/orders(.*)/, methods: ['GET', 'POST', 'OPTIONS'] },
+            { url: /\/api\/search(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/user\/google-login(.*)/, methods: ['POST', 'OPTIONS'] },
+            { url: /\/api\/user\/verifyemail(.*)/, methods: ['POST', 'OPTIONS'] },
+
+            // Fix: Inhe Regex format mein daalo taaki accurate matching ho sake
+            { url: /\/api\/user\/forgotPassword(.*)/, methods: ['POST', 'OPTIONS'] },
+            { url: /\/api\/user\/resetPassword(.*)/, methods: ['POST', 'OPTIONS'] },
         ]
     });
 }
